b4d012ce219af0c5787669037362c62d12fe053c
Shreyansh Sharma <shreyansh102000@gmail.com>
2024-09-30 16:28:32 -0400

    Create routePath.js

---
+++ Frontend/src/Pages/GlobalFunctions/routePath.js
@@ -0,0 +1,22 @@
+const baseURL = "http://localhost:5173/";
+
+export const ROUTE_PATH = {
+  HOME_PAGE: "/",
+  ADMIN_DASHBOARD: "/admin_dashboard",
+  DASHBOARD: "/dashboard",
+  DASHBOARD_ID: "/dashboard/:dashboardID",
+  ADMIN_LOGIN: "/admin_login",
+  ENROLL_CHILD: "/dashboard/enroll_child",
+  MANAGE_STAFF: "/dashboard/manage_staff",
+  MANAGE_CLASSROOM: "/dashboard/manage_classroom",
+  ATTENDANCE: "/dashboard/attendance",
+  ACCOUNTING_LEDGER: "/dashboard/accounting_ledger",
+  REPORTS: "/dashboard/reports",
+  REPORTS_ATTENDANCE: "/dashboard/reports/attendance",
+  REPORTS_LEDGER: "/dashboard/reports/ledger",
+  CREATE_ACCOUNT: "/create_account",
+  PERSONAL_INFORMATION: "/dashboard/personal_information",
+  ATTENDANCE_ACCOUNTING: "/dashboard/attendance_accounting",
+  CHILD_ATTENDANCE: "/dashboard/child_attendance",
+  FACILITY_INFORMATION: "/dashboard/facility_information",
+};
